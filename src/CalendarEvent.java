699bec8674054d1ff089b9608a604b5064620f4b
brusqueboi <jdalton@hawaii.edu>
2016-02-24 23:10:23 -1000

    change

--- src/CalendarEvent.java
+++ src/CalendarEvent.java
@@ -14,117 +14,34 @@ public class CalendarEvent {
 //***********************************************************************************************
 //  Declaration of variables
 //***********************************************************************************************
-    // Scanner to read user input
-    Scanner sc = new Scanner (System.in);
-
-    // strings for the date and the correct format
-    final String uformat = "MM/dd/yyyy";
-    final String cformat = "yyyy/MM/dd";
-
-    // declarations of variables used for the .ics file
-    boolean end = false;
-    int choice = 0;
-    File file = null;
-    String ver = "VERSION:", pvt = "PRIVATE", pub = "PUBLIC", con = "CONFIDENTIAL";
-    double verNum = 2.0;
-    String timez = "", tx = "", dtstart = "", dtend = "", upper = "";
-    String location = "", summary = "", input = "", cl = "", priority = "";
-    String newStartDateStr  = "", newEndDateStr = "";
-    String[] splitTime, splitDate;
-    String sTime = "", eTime = "", sHTime = "", sMTime = "", eHTime = "", eMTime = "", sDate = "", eDate = "";
-    int intETime, intSTime, intSHTime, intSMTime, intEHTime, intEMTime, intPriority;
-
+    File file;
 //***********************************************************************************************
 //  start of code
 //***********************************************************************************************
+    try{
 
-    ver = ver.concat(verNum + "\n");
+      // create ics file
+      file = new File("calendarevent.ics");
+      BufferedWriter output = new BufferedWriter(new FileWriter(file));
 
-    System.out.println("\nThis program is to create an event that gets exported");
-    System.out.println("to a .ics file and then could be imported to a calendar");
-    System.out.println("application.");
+      // write to file
+      output.write("BEGIN:VCALENDAR\n");
+      output.write("VERSION: 2.0\n");
+      output.write("BEGIN:VEVENT\n");
+      output.write("DTSTART;TZID=Pacific/Honolulu:20160226T130000\n");
+      output.write("DTEND;TZID=Pacific/Honolulu:20160226T140000\n");
+      output.write("LOCATION: UH Manoa ICSpace\n");
+      output.write("SUMMARY: meeting\n");
+      output.write("END:VEVENT\n");
+      output.write("END:VCALENDAR\n");
 
-    while(end == false) {
+      output.close();
 
-      System.out.println("\nPlease pick from the following options to create an");
-      System.out.println("event: ");
-      System.out.println("\n1. Single day event \n2. Multiple day event \n3. quit");
-
-      input = sc.nextLine();
-
-      try {
-          choice = Integer.parseInt(input);
-          if(choice<4){
-
-            switch(choice){
-
-                case 1:
-
-                    // user input for the name of the event
-                    System.out.println("\nYou have chosen a single day event.");
-                    System.out.println("Event name: ");
-                    input = sc.nextLine();
-                    summary = summary.concat(input+"\n");
-
-                    // user input for the location of event
-                    System.out.println("\nLocation of Event: ");
-                    input = sc.nextLine();
-                    location = location.concat(input+"\n");
-
-                    // user input for the date of event
-                    System.out.println("\nEnter date (MM/DD/YYYY) of event: ");
-                    input = sc.nextLine();
-
-                    // formats the date
-                    SimpleDateFormat sdf = new SimpleDateFormat(uformat);
-                    try {
-
-                        // changes date to match the calendar formatting
-                        Date d = new Date();
-                        d = sdf.parse(input);
-                        sdf.applyPattern(calFormat);
-                        newStartDateStr = sdf.format(d);
-
-                        // removes all the things not used in the calendar format
-                        input = newStartDateStr.replaceAll("/", "");
-                        dtstart = dtstart.concat(input);
-                        dtend = dtstart;
-
-                    }
-                    catch(ParseException pe){
-
-                        // helps find problems in code, if any
-                        pe.printStackTrace();
-                    }
-
-                    // user input for the  start and end time of the event
-                    System.out.println("\nStart time (ex. 8am or 8:30am) of the event: ");
-                    sTime = sc.nextLine();
-                    System.out.println("\nEnd time (ex. 10am or 10:30am) of the event: ");
-                    eTime = sc.nextLine();
-
-                    // format time for calendar format
-                    if(sTime.contains("am")){
-
-                        sTime = sTime.replace("am", "");
-
-                        if(sTime.contains(":")){
-
-                            splitTime = sTime.split(":");
-                            
-                        }
-                    }
-
-            }
-          }
-
-      }
-      catch (java.lang.NumberFormatException se){
-
-        System.out.println("\nYou have entered an invalid selection.");
-        System.out.println("\nPlease try again.");
-      }
     }
+    catch(Exception e){
+      System.out.print("Error");
+    }
+
     System.exit(0);
   }
 }
