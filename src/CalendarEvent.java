d86ac82714eff0f3f3510522bd77dcf6a9780c58
brusqueboi <jdalton@hawaii.edu>
2016-02-24 23:10:14 -1000

    Update CalendarEvent.java

--- src/CalendarEvent.java
+++ src/CalendarEvent.java
@@ -1,15 +1,7 @@
 //***********************************************************************************************
 //  Team: Doze
 //  ICS 314
-<<<<<<< HEAD
 //  Authors: Dorienne Matsuno, Marlene Mabut, Jacob Dalton
-=======
-<<<<<<< HEAD
-//  Authors: Dorienne Matsuno, Marlene Abut, Jacob Dalton
-=======
-//  Authors: Dorienne Matsuno, Marlene Abut, Jacob Dalton
->>>>>>> origin/master
->>>>>>> origin/master
 //
 //***********************************************************************************************
 import java.util.*;
