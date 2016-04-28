9a3f1013f2da634bd7c4449e4b3e1835dbabc321
dmatsuno <dmatsuno@hawaii.edu>
2016-04-27 18:51:08 -1000

    adding to the implementation of the insert method of the linked list class

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -11,6 +11,15 @@ public class LinkedList {
 		size = 0;
 	}
 	public void insert(Object data, int index){
-		
+		Node temp = new Node(data);
+		Node current = head;
+		if(current != null){
+			for(int i = 0; i < index && current.getNext() != null; i++){
+				current = current.getNext();
+			}
+		}
+		temp.setNext(current.getNext());
+		current.setNext(temp);
+		size++;
 	}
 }
